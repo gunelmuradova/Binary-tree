5801abb431a29f1b277a97d5d97ca3b8044d278c
gunelmuradova <71687988+gunelmuradova@users.noreply.github.com>
2020-11-18 02:22:20 -0800

    Add files via upload

---
+++ binarytree_task.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+	int value;
+	struct Node* left;
+	struct Node* right;
+};
+struct Node* newNode(int value)
+{
+	struct Node* node = (struct Node*) malloc(sizeof(struct Node));
+	node->value = value;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
+}
+int addNodes(struct Node* iter)
+{
+	if (iter == NULL)
+		return 0;
+	if(addNodes(iter->right) > addNodes(iter->left))
+		return iter->value + addNodes(iter->right);
+	else
+	return iter->value + addNodes(iter->left);
+}
+
+
+/*
+ *        1
+ *     5       10
+ *  20  4   21     20
+ *999     13        15
+ * */
+
+int main(){
+	struct Node* root = newNode(1);
+	root->left = newNode(5);
+	root->left->left = newNode(20);
+	root->left->left->left = newNode(999);
+	root->left->right = newNode(4);
+	root->left->right->right = newNode(13);
+	root->right = newNode(10);
+	root->right->left = newNode(21);
+	root->right->right = newNode(20);
+	root->right->right->right = newNode(15);
+
+	printf("sum of tree nodes: %d",addNodes(root));
+	return 0;
+}
